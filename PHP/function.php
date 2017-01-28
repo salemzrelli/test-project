2f53ba8609fa17e35a257315dd32770aa57d3e73
Mohamed ali Tlili <mohamedali.tlili@esprit.tn>
2017-01-28 23:21:54 +0100

    isogram function7

--- PHP/function.php
+++ PHP/function.php
@@ -1,11 +1,26 @@
 <?php
 
+
+//isogram upercase function
 function test($input){
     $result="";
     for ($i==0;i<strlen($input);i+2){
         $result=$result. strtoupper($input[i]);
     }
     return $result;
+}
+
+//isogram function
+
+function isIsogram(string $initialString) {
+    $encounteredCharacters = [];
+    foreach(prepareString($initialString) as $char) {
+        if (in_array($char, $encounteredCharacters)) {
+            return false;
+        }
+        $encounteredCharacters[] = $char;
+    }
+    return true;
 }
 
 ?>
